51a98832945967f0952c2e72e59127e1abf73471
florian dahlitz <f2dahlitz@freenet.de>
2017-01-03 18:03:00 +0100

    adding top navbar for anything but home site

--- app/views/header.php
+++ app/views/header.php
@@ -11,61 +11,31 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 </head>
 <body>
     <div id="wrapper">
-		<header id="header">
-			<span class="avatar"><img src="<?php echo jpg_file_url('avatar'); ?>" alt="<?php echo $this->config->item('name'); ?>" /></span>
-			<h1><?php echo nl2br($start_text->text); ?></h1>
-			<ul class="icons">
-				<li><a href="#" class="icon style2 fa-twitter"><span class="label">Twitter</span></a></li>
-				<li><a href="#" class="icon style2 fa-facebook"><span class="label">Facebook</span></a></li>
-				<li><a href="#" class="icon style2 fa-instagram"><span class="label">Instagram</span></a></li>
-				<li><a href="#" class="icon style2 fa-envelope-o"><span class="label">Email</span></a></li>
-			</ul>
-		</header>
-		<section id="main">
-			<section class="thumbnails">
-				<div>
-					<a href="images/fulls/01.jpg">
-						<img src="images/thumbs/01.jpg" alt="" />
-						<h3>Lorem ipsum dolor sit amet</h3>
-					</a>
-					<a href="images/fulls/02.jpg">
-						<img src="images/thumbs/02.jpg" alt="" />
-						<h3>Lorem ipsum dolor sit amet</h3>
-					</a>
-				</div>
-				<div>
-					<a href="images/fulls/03.jpg">
-						<img src="images/thumbs/03.jpg" alt="" />
-						<h3>Lorem ipsum dolor sit amet</h3>
-					</a>
-					<a href="images/fulls/04.jpg">
-						<img src="images/thumbs/04.jpg" alt="" />
-						<h3>Lorem ipsum dolor sit amet</h3>
-					</a>
-					<a href="images/fulls/05.jpg">
-						<img src="images/thumbs/05.jpg" alt="" />
-						<h3>Lorem ipsum dolor sit amet</h3>
-					</a>
-				</div>
-				<div>
-					<a href="images/fulls/06.jpg">
-						<img src="images/thumbs/06.jpg" alt="" />
-						<h3>Lorem ipsum dolor sit amet</h3>
-					</a>
-					<a href="images/fulls/07.jpg">
-						<img src="images/thumbs/07.jpg" alt="" />
-						<h3>Lorem ipsum dolor sit amet</h3>
-					</a>
-				</div>
-			</section>
-		</section>
-    	<footer id="footer">
-			<p>&copy; <?php echo $this->config->item('name'); ?>. Alle Rechte vorbehalten. Design: <a href="http://templated.co">TEMPLATED</a>.</p>
-		</footer>
-	</div>
-
-    <!-- Other Scripts -->
-	<script src="<?php echo js_file_url('jquery.min'); ?>"></script>
-    <script src="<?php echo js_file_url('jquery.poptrox.min'); ?>"></script>
-	<script src="<?php echo js_file_url('skel.min'); ?>"></script>
-	<script src="<?php echo js_file_url('main'); ?>"></script>
+        <header id="header">
+            <span class="avatar"><img src="<?php echo jpg_file_url('avatar'); ?>" alt="<?php echo $this->config->item('name'); ?>" /></span>
+            <h1>
+                Willkommen auf meinem Fotoblog!<br>
+                Hier werde ich regelmäßig neue Bilder hochladen. Enjoy!
+            </h1>
+            <ul class="icons">
+                <li><a href="#" class="icon style2 fa-twitter"><span class="label">Twitter</span></a></li>
+                <li><a href="#" class="icon style2 fa-facebook"><span class="label">Facebook</span></a></li>
+                <li><a href="#" class="icon style2 fa-instagram"><span class="label">Instagram</span></a></li>
+                <li><a href="<?php echo base_url('contact'); ?>" class="icon style2 fa-envelope-o"><span class="label">Email</span></a></li>
+            </ul>
+        </header>
+        <nav id="head-nav">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Artikel</a>
+                    <ul>
+                        <li><a href="#">beliebteste Artikel</a></li>
+                        <li><a href="#">letzte Artikel</a></li>
+                        <li><a href="#">alle Artikel</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Kategorien</a></li>
+                <li><a href="#">About me</a></li>
+                <li><a href="#">Kontakt</a></li>
+            </ul>
+        </nav>
