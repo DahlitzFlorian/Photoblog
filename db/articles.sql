1ea2afc255d278110463058244d80fe4fc00e244
florian dahlitz <f2dahlitz@freenet.de>
2017-01-03 19:28:33 +0100

    added 'articles'-table structure as sql file

---
+++ db/articles.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 03. Jan 2017 um 19:17
+-- Server Version: 5.6.21
+-- PHP-Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Datenbank: `foto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `articles`
+--
+
+CREATE TABLE IF NOT EXISTS `articles` (
+`id` int(6) unsigned NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `text` text NOT NULL,
+  `author` varchar(100) NOT NULL DEFAULT 'Standard',
+  `path` varchar(150) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Daten für Tabelle `articles`
+--
+
+INSERT INTO `articles` (`id`, `title`, `text`, `author`, `path`, `date`) VALUES
+(1, 'Example1', 'Cool text and pics', 'Standard', '2017/test', '2017-01-03');
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `articles`
+--
+ALTER TABLE `articles`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `articles`
+--
+ALTER TABLE `articles`
+MODIFY `id` int(6) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
