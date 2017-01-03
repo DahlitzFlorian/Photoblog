248f1eb275579b4aeadf656f5283a4b12f5333ee
florian dahlitz <f2dahlitz@freenet.de>
2017-01-03 17:44:12 +0100

    updated footer

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,2 +1,13 @@
+        <footer id="footer">
+            <p>&copy; <?php echo $this->config->item('name'); ?>. Alle Rechte vorbehalten. <a href="<?php echo base_url('contact'); ?>">Kontakt</a>. <a href="<?php echo base_url('articles/imprint'); ?>">Impressum</a>. <a href="<?php echo base_url('admin'); ?>">Login</a>.</p>
+        </footer>
+    </div>
+    
+     <!-- Other Scripts -->
+    <script src="<?php echo js_file_url('jquery.min'); ?>"></script>
+    <script src="<?php echo js_file_url('jquery.poptrox.min'); ?>"></script>
+    <script src="<?php echo js_file_url('skel.min'); ?>"></script>
+    <script src="<?php echo js_file_url('main'); ?>"></script>
+    
 </body>
 </html>
