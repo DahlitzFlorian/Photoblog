327e05b374c3fb9065d618fd22cdf8bcd8d13905
florian dahlitz <f2dahlitz@freenet.de>
2017-01-08 20:52:09 +0100

    updated social networking

--- app/views/header.php
+++ app/views/header.php
@@ -19,17 +19,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <nav id="sidenav" class="sidenav">
             <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
             <ul>
-                <li><a href="#">Home</a></li>
+                <li><a href="<?php echo base_url(); ?>">Home</a></li>
                 <li id="submenu1"><a href="#">Artikel</a>
                     <ul>
-                        <li><a href="#">beliebteste Artikel</a></li>
-                        <li><a href="#">letzte Artikel</a></li>
-                        <li><a href="#">alle Artikel</a></li>
+                        <li><a href="<?php echo base_url('article/latest'); ?>">letzte Artikel</a></li>
+                        <li><a href="<?php echo base_url('article/all'); ?>">alle Artikel</a></li>
                     </ul>
                 </li>
-                <li><a href="#">Kategorien</a></li>
-                <li><a href="#">About me</a></li>
-                <li><a href="#">Kontakt</a></li>
+                <li><a href="<?php echo base_url('category'); ?>">Kategorien</a></li>
+                <li><a href="<?php echo base_url('article/show/aboutme'); ?>">About me</a></li>
+                <li><a href="<?php echo base_url('contact'); ?>">Kontakt</a></li>
             </ul>
         </nav>
         <div id="mainmain">
@@ -40,9 +39,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     Hier werde ich regelmäßig neue Bilder hochladen. Enjoy!
                 </p>
                 <ul class="icons">
-                    <li><a href="#" class="icon style2 fa-twitter"><span class="label">Twitter</span></a></li>
-                    <li><a href="#" class="icon style2 fa-facebook"><span class="label">Facebook</span></a></li>
-                    <li><a href="#" class="icon style2 fa-instagram"><span class="label">Instagram</span></a></li>
+                    <li><a href="https://twitter.com/Chr1sVanTun" class="icon style2 fa-twitter"><span class="label">Twitter</span></a></li>
+                    <li><a href="https://www.facebook.com/christoph.dahlitz" class="icon style2 fa-facebook"><span class="label">Facebook</span></a></li>
+                    <li><a href="https://www.instagram.com/chris_van_tun/" class="icon style2 fa-instagram"><span class="label">Instagram</span></a></li>
                     <li><a href="<?php echo base_url('contact'); ?>" class="icon style2 fa-envelope-o"><span class="label">Email</span></a></li>
                 </ul>
             </header>
