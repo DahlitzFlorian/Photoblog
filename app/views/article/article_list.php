f16f4ed769c5a11f94b2d0821e66301064e48413
florian dahlitz <f2dahlitz@freenet.de>
2017-01-15 20:16:08 +0100

    added + removed useless code

--- app/views/article/article_list.php
+++ app/views/article/article_list.php
@@ -2,7 +2,7 @@
             <h2><?php echo $header; ?></h2>
                 <?php foreach($articles as $article): ?>
                     <a href="<?php echo base_url('article/show/' . $article->path); ?>"><article class="article_list">
-                        <div class="thumbnail"><img src="<?php echo article_pics_url() . $article->path . '/thumbnail.jpg'; ?>"></img></div>
+                        <div class="thumbnail"><img src="<?php echo article_pics_url() . $article->path . '/thumbnail.jpg'; ?>"></img></div>                        
                         <h4><?php echo $article->title; ?></h4>
                         <?php 
                             if (strlen($article->text) > 90): $article->text = substr($article->text, 0, 86) . ' ...';
