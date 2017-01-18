a84864525dc074a36597f10794299a75278d5f5a
florian dahlitz <f2dahlitz@freenet.de>
2017-01-18 21:02:58 +0100

    general admin header/footer adds

---
+++ app/views/admin/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="<?php echo $this->config->item('charset'); ?>">
+    
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+    <div id="wrapper">
+        <header class="top-header"></header>
+        <nav class="site-nav">
+            <ul>
+                <li><a href="#">Dashboard</a></li>
+                <li><a href="#">Nutzer</a></li>
+                <li><a href="#">Artikel</a></li>
+                <li><a href="#">Todo-List</a></li>
+            </ul>
+        </nav>
+        <section class="main">
