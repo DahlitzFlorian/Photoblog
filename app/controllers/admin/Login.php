8c1acae8385ffa377bf8d08afd500d82e53ad3c4
florian dahlitz <f2dahlitz@freenet.de>
2017-01-21 22:23:26 +0100

    set up validation rules w/out checking db

--- app/controllers/admin/Login.php
+++ app/controllers/admin/Login.php
@@ -16,6 +16,35 @@ class Login extends MY_Controller
     
     public function index()
     {
+        if($this->input->post('login_submit'))
+        {
+            $this->load->library('form_validation');
+            $this->form_validation->set_rules([
+                [
+                    'field' => 'username',
+                    'Label' => 'Nutzername',
+                    'rules' => 'required',
+                    'errors' => []
+                ],
+                [
+                    'field' => 'password',
+                    'label' => 'Passwort',
+                    'rules' => 'required',
+                    'errors' => []
+                ]
+            ]);
+            
+            if($this->form_validation->run())
+            {
+                // set up session data
+                redirect(base_url('admin/dashboard'));
+            }
+            else
+            {
+                // create error msg
+            }
+        }
+        
         $this->load->view('admin/login', $this->data);
     }
 }
--- app/views/admin/login.php
+++ app/views/admin/login.php
@@ -20,27 +20,29 @@
             <h2>Venture Dahlitz | Fotoblog</h2>
         </div>
         <div class="right">
-            <div class="login-wrap">
-                <div class="login-html">
-                    <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Login</label>
-                    <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" id="tab" class="tab">Sign Up</label>
-                    <div class="login-form">
-                        <div class="sign-in-htm">
-                            <div class="group">
-                                <label for="user" class="label">Nutzername</label>
-                                <input id="user" name="username" type="text" class="input">
-                            </div>
-                            <div class="group">
-                                <label for="pass" class="label">Passwort</label>
-                                <input id="pass" name="password" type="password" class="input" data-type="password">
-                            </div>
-                            <div class="group">
-                                <input type="submit" class="button" value="Anmelden">
+            <form action="<?php echo base_url('admin/login'); ?>" name="login_form" method="post">
+                <div class="login-wrap">
+                    <div class="login-html">
+                        <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Login</label>
+                        <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" id="tab" class="tab">Sign Up</label>
+                        <div class="login-form">
+                            <div class="sign-in-htm">
+                                <div class="group">
+                                    <label for="user" class="label">Nutzername</label>
+                                    <input id="user" name="username" type="text" class="input">
+                                </div>
+                                <div class="group">
+                                    <label for="pass" class="label">Passwort</label>
+                                    <input id="pass" name="password" type="password" class="input" data-type="password">
+                                </div>
+                                <div class="group">
+                                    <input name="login_submit" type="submit" class="button" value="Anmelden">
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </form>
         </div>
     </div>
 </body>
