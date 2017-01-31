c73e00b050a41f192a8fe5ac655c91f0b5272d58
florian dahlitz <f2dahlitz@freenet.de>
2017-01-31 17:05:12 +0100

    completed general dashboard view

--- app/views/admin/dashboard.php
+++ app/views/admin/dashboard.php
@@ -1,1 +1,5 @@
-<?php
+            <h1>Dashboard</h1>
+            <div class="box">
+                <h4 class="bg-lightblue">Willkommen</h4>
+                <p>Guten Tag, <?php echo $user->first_name . ' ' . $user->last_name; ?>.</p>
+            </div>
