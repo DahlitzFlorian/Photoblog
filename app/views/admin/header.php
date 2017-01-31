4d76cca6465872307e2f3b67fbc5f150c412c0f6
florian dahlitz <f2dahlitz@freenet.de>
2017-01-31 17:06:22 +0100

    updated side navigation links

--- app/views/admin/header.php
+++ app/views/admin/header.php
@@ -5,18 +5,22 @@
     
     <title><?php echo $title; ?></title>
     
+    <!-- Own Stylesheets -->
     <link rel="stylesheet" href="<?php echo css_file_url('admin/reset'); ?>" type="text/css">
     <link rel="stylesheet" href="<?php echo css_file_url('admin/main'); ?>" type="text/css">
+    <link rel="stylesheet" href="<?php echo css_file_url('helper'); ?>" type="text/css">
 </head>
 <body>
     <div id="wrapper">
-        <header class="top-header"></header>
-        <nav class="site-nav">
+        <nav class="side-nav">
+            <span>VD-Admin</span>
             <ul>
                 <li><a href="<?php echo base_url('admin/dashboard'); ?>">Dashboard</a></li>
                 <li><a href="<?php echo base_url('admin/user'); ?>">Nutzer</a></li>
                 <li><a href="<?php echo base_url('admin/article'); ?>">Artikel</a></li>
                 <li><a href="<?php echo base_url('admin/todo'); ?>">Todo-List</a></li>
+                <li><a href="<?php echo base_url('admin/settings'); ?>">Einstellungen</a></li>
+                <li><a href="<?php echo base_url('admin/login/logout'); ?>">Logout</a></li>
             </ul>
         </nav>
         <section class="main">
