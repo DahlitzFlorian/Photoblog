a738fd5592e7cd66399e67148fd017c3a9da5a92
florian dahlitz <f2dahlitz@freenet.de>
2017-02-02 19:30:23 +0100

    reception on dashboard now dynamically changed by current hour

--- app/controllers/admin/Dashboard.php
+++ app/controllers/admin/Dashboard.php
@@ -19,6 +19,24 @@ class Dashboard extends MY_Controller
     
     public function index()
     {
+        $reception = [
+            1 => 'Guten Morgen',
+            2 => 'Guten Tag',
+            3 => 'Guten Abend',
+            4 => 'Es ist spät - Hallo'
+        ];
+        
+        $curr_hour = date('H');
+        
+        if($curr_hour >= 5 && $curr_hour < 9)
+            $this->data['reception'] = $reception[1];
+        else if($curr_hour >= 9 && $curr_hour < 18)
+            $this->data['reception'] = $reception[2];
+        else if($curr_hour >= 18 && $curr_hour < 22)
+            $this->data['reception'] = $reception[3];
+        else 
+            $this->data['reception'] = $reception[4];
+        
         $this->data['subview'] = 'admin/dashboard';
         $this->load->view('admin/layout', $this->data);
     }
--- app/views/admin/dashboard.php
+++ app/views/admin/dashboard.php
@@ -1,5 +1,5 @@
             <h1>Dashboard</h1>
             <div class="box">
                 <h4 class="bg-lightblue">Willkommen</h4>
-                <p>Guten Tag, <?php echo $user->first_name . ' ' . $user->last_name; ?>.</p>
+                <p><?php echo $reception; ?>, <?php echo $user->first_name . ' ' . $user->last_name; ?>.</p>
             </div>
