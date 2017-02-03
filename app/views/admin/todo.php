63b96275d8ef85bf81b08d933dcb802176c2dabf
florian dahlitz <f2dahlitz@freenet.de>
2017-02-03 19:20:51 +0100

    added action 'dashboard lickage' to todo overview

--- app/views/admin/todo.php
+++ app/views/admin/todo.php
@@ -5,7 +5,7 @@
             <?php foreach($todos as $todo): ?>
             <div class="box">
                 <h4 class="bg-darkblue white"><?php echo $todo->title; ?></h4>
-                <span class="actions"><a href="<?php echo base_url('admin/todo/dash_linkage/' . $todo->id); ?>"><?php echo ($todo->dash_link == 0) ? 'An Dashboard anheften' : 'Von Dashboard lösen'; ?></a> - <a href="<?php echo base_url('admin/todo/edit/' . $todo->id); ?>">Bearbeiten</a> - <a href="<?php echo base_url('admin/todo/delete/' . $todo->id); ?>">Löschen</a></span>
+                <span class="actions"><a href="<?php echo base_url('admin/todo/dash_linkage/' . $todo->id); ?>"><?php echo ($todo->dash_link == 0) ? 'An Dashboard anheften' : 'Von Dashboard lösen'; ?></a> - <a href="<?php echo base_url('admin/todo/archive/' . $todo->id); ?>">Archivieren</a> - <a href="<?php echo base_url('admin/todo/edit/' . $todo->id); ?>">Bearbeiten</a> - <a href="<?php echo base_url('admin/todo/delete/' . $todo->id); ?>">Löschen</a></span>
                 <span class="details">Autor: <?php echo $todo->author; ?></span>
                 <p><?php echo nl2br($todo->content); ?></p>
             </div>
