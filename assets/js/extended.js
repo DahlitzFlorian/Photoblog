f75ddaa092773f621061b462c0a5675ed31c4f81
florian dahlitz <f2dahlitz@freenet.de>
2017-06-28 18:59:44 +0200

    fixed issue #4 --> responsive navigation

--- assets/js/extended.js
+++ assets/js/extended.js
@@ -2,13 +2,13 @@ if (Modernizr.mq('(max-width: 516px)')) {
     /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
     function openNav() {
         document.getElementById("sidenav").style.width = "100%";
-        document.getElementById("wrapper").style.marginLeft = "250px";
+        document.getElementById("wrapper").style.width = "0";
     }
 
     /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
     function closeNav() {
         document.getElementById("sidenav").style.width = "0";
-        document.getElementById("wrapper").style.marginLeft = "auto";
+        document.getElementById("wrapper").style.width = "auto";
     }
 } 
 else {
